98737b5093aaa85435ecc4d86ad49153e488c1e4
Delfim Marime <Delfimarime@gmail.com>
2025-07-13 10:42:56 +0200

    wip: entities definition

---
+++ engine.go
@@ -0,0 +1,12 @@
+package aegis
+
+type EvaluateResponse struct {
+}
+
+type Aegis struct {
+	datasource Datasource
+}
+
+func (instance *Aegis) evaluate(resource Resource) (EvaluateResponse, error) {
+	return EvaluateResponse{}, nil
+}
